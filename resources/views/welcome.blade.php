cd536bb8f30c542bd0957b50f2e6085bff6f9232
Younes <faouzi.younes@hotmail.com>
2020-04-22 11:00:37 +0200

    Projet+Create+Controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.index')
 @section('content')
 
+
+
+<div class="row ">
+
+    <a href="/create" class="btn btn-success">Create</a>
+
+    @foreach ($projets as $projet) --}}
+    <div class="col-1">ID : {{$projet->id}} </div>
+    <div class="col-3">Nom Projet : {{$projet->name}} </div>
+    <div class="col-6">Description : {{$projet->description}} </div> --}}
+    <div class="col-2">TEST</div>
+    @endforeach
+</div>
+    
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::resource('/projets', 'ProjetController');
+Route::resource('/', 'ProjetController');
+
+
